784c5075acea64a2a97d8712ca843b458efe6985
silky <noonsilk+-noonsilk@gmail.com>
2019-01-22 11:09:31 +1300

    empty start

--- src/Main.hs
+++ src/Main.hs
@@ -15,6 +15,37 @@ import Diagrams.TwoD.Path.Boolean
 main :: IO ()
 main = mainWith (frame 0.2 <$> d) >> putStrLn "Done!"
 
+
+d :: IO (Diagram B)
+d = do
+  return $ mempty
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+haskellPoints :: [[(Double, Double)]]
 haskellPoints = [ -- >
                   [ (0, 3), (1.2, 1.5), (0,0) ]
                   -- \
@@ -36,52 +67,4 @@ haskellLogo = Path trails
     where
         verts  = (map . map) p2 haskellPoints
         trails = map fromVertices verts 
-
-
-bars :: Diagram B
-bars = vsep 0.5 (replicate 3 $ rect 10 1)
-        # centerXY
-        # scaleY 0.5
-
-
-dropShadow :: Diagram B -> Diagram B
-dropShadow d = d <> d # (translateX 0.4)
-                      # (translateY (-0.4))
-                      # fc black
-
-
-doTransform :: Diagram B -> IO (Diagram B)
-doTransform d = do
-  -- Scale
-  s <- randomRIO (0.3, 1.2)
-
-  -- Rotation
-  r <- randomRIO (0, 360)
-
-  -- Colour
-  c <- randomRIO (0, len)
-
-
-  return $ d # scale s
-             # rotateBy r
-             # fc (colours !! c)
-             # lw none
-    where
-      colours = brewerSet Set3 12
-      len     = length colours - 1
-
-
-d :: IO (Diagram B)
-d = do
-  let items = [ rect 8 1
-              , circle 3
-              , triangle 3
-              , bars
-              , haskellLogo
-              ]
-
-  -- let diags = map (fc blue . dropShadow) items
-  diags <- mapM (doTransform . dropShadow) items
-  
-  return $ hcat diags
 
