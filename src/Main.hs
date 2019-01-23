0e4316cab7f92161a4f319bba1160460f4af25d9
silky <noonsilk+-noonsilk@gmail.com>
2019-01-24 00:47:44 +1100

    code from the talk

--- src/Main.hs
+++ src/Main.hs
@@ -16,9 +16,42 @@ main :: IO ()
 main = mainWith (frame 0.2 <$> d) >> putStrLn "Done!"
 
 
+bars :: Diagram B
+bars = vsep 0.5 (replicate 3 (rect 10 1))
+          # centerXY
+          # scaleY 0.5
+
+
+dropShadow :: Diagram B -> Diagram B
+dropShadow d = d <> d # translateX 0.4
+                      # translateY (-0.4)
+                      # fc black
+
+doTransform :: Diagram B -> IO (Diagram B)
+doTransform d = do
+  s <- randomRIO (0.3, 1.2)
+  r <- randomRIO (0,   360)
+  c <- randomRIO (0, len)
+
+  return $ d # scale s
+             # rotateBy r
+             # fc (colours !! c)
+  where
+    colours = brewerSet Set3 12
+    len = length colours - 1
+
+
 d :: IO (Diagram B)
 d = do
-  return $ mempty
+  let items = [ circle 3
+              , triangle 2
+              , bars
+              , haskellLogo
+              ]
+
+  xs <- mapM (doTransform . dropShadow) items
+  return $ hcat xs
+  -- return $ hcat (map (fc blue . dropShadow) items)
 
 
 
